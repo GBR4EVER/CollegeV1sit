6c02c3308b002f05d94b0971fd691b53e86d8951
Corey <burkleycorey@yahoo.com>
2015-11-18 12:18:01 -0600

    Added new validations for User model and moved action methods to the controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,10 @@
 class User < ActiveRecord::Base
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      flash[:notice] = "You signed up correctly!"
-      flash[:color] = "valid"
-    else
-      flash[:notice] = "The form is invalid."
-      flash[:color] = "invalid"
-    end
-    
-    render "new"
-    
-  end
-
+  
+  attr_accessor :password
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+  validates :password, :confirmation => true #password_confirmation attr
+  validates_length_of :password, => :in 6..20, :on => :create
+  
 end
