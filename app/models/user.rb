b381b2c2d40b5cab84e06fc118658b1181ceecc8
Corey <burkleycorey@yahoo.com>
2015-11-18 11:36:05 -0600

    Added some new and create methods through the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "You signed up correctly!"
+      flash[:color] = "valid"
+    else
+      flash[:notice] = "The form is invalid."
+      flash[:color] = "invalid"
+    end
+    
+    render "new"
+    
+  end
+
 end
